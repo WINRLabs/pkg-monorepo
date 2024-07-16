c28db329ced3f4302d9ffda0b54beda38c211531
WINRLabs <justb.serbia@gmail.com>
2024-07-16 11:50:09 +0300

    ✨ (horse-race): Add horse race participant map with store for different horses and their multipliers 🔧 (use-contract-config): Add horse race contract address to the contract config context 🔧 (horse-race): Update horse race game to include new horse race contract address in the game addresses 🔧 (horse-race): Update horse race game to include horse race in the game hub games enum 🔧 (horse-race): Update horse race game to prepare game transaction with horse race specific parameters 🔧 (horse-race): Update horse race game to handle transactions for horse race game 🔧 (horse-race): Update horse race game to handle claim transactions for horse race game 🔧 (horse-race): Update horse race game to handle game events and update game state accordingly 🔧 (utils): Update game hub games enum to include horse race with hyphen instead of underscore 🔧 (utils): Update prepare game transaction function to remove unnecessary commas and fix formatting

--- packages/web3-games/src/games/horse-race/index.tsx
+++ packages/web3-games/src/games/horse-race/index.tsx
@@ -1,14 +1,25 @@
-import { HorseRaceFormFields, HorseRaceTemplate } from "@winrlabs/games";
 import {
+  Horse,
+  HorseRaceFormFields,
+  HorseRaceStatus,
+  HorseRaceTemplate,
+  MultiplayerGameStatus,
+  participantMapWithStore,
+  useHorseRaceGameStore,
+} from "@winrlabs/games";
+import {
+  controllerAbi,
   useCurrentAccount,
+  useHandleTx,
   useTokenAllowance,
   useTokenStore,
 } from "@winrlabs/web3";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { Address, encodeAbiParameters, encodeFunctionData } from "viem";
 
 import { useListenMultiplayerGameEvent } from "../hooks";
 import { useContractConfigContext } from "../hooks/use-contract-config";
-import { GAME_HUB_GAMES } from "../utils";
+import { GAME_HUB_GAMES, prepareGameTransaction } from "../utils";
 
 type TemplateOptions = {
   scene?: {
@@ -35,8 +46,19 @@ const HorseRaceGame = (props: TemplateWithWeb3Props) => {
   const selectedToken = useTokenStore((s) => s.selectedToken);
   const selectedTokenAddress = selectedToken.address;
 
-  const [formValues, setFormValues] = useState<HorseRaceFormFields>();
+  const [formValues, setFormValues] = useState<HorseRaceFormFields>({
+    horse: Horse.IDLE,
+    wager: 1,
+  });
+
+  const { updateState, setSelectedHorse } = useHorseRaceGameStore([
+    "updateState",
+    "setSelectedHorse",
+  ]);
+
   const gameEvent = useListenMultiplayerGameEvent(GAME_HUB_GAMES.horse_race);
+
+  console.log("gameEvent", gameEvent);
 
   const currentAccount = useCurrentAccount();
 
@@ -46,6 +68,117 @@ const HorseRaceGame = (props: TemplateWithWeb3Props) => {
     spender: cashierAddress,
     tokenAddress: selectedTokenAddress,
     showDefaultToasts: false,
+  });
+
+  const encodedParams = useMemo(() => {
+    const { tokenAddress, wagerInWei } = prepareGameTransaction({
+      wager: formValues?.wager || 0,
+      stopGain: 0,
+      stopLoss: 0,
+      selectedCurrency: "0x0000000000000000000000000000000000000001",
+      lastPrice: 1,
+    });
+
+    const encodedGameData = encodeAbiParameters(
+      [
+        { name: "wager", type: "uint128" },
+        { name: "horse", type: "uint8" },
+      ],
+      [wagerInWei, formValues.horse as any]
+    );
+
+    const encodedData: `0x${string}` = encodeFunctionData({
+      abi: controllerAbi,
+      functionName: "perform",
+      args: [
+        gameAddresses.horseRace as Address,
+        tokenAddress,
+        uiOperatorAddress as Address,
+        "bet",
+        encodedGameData,
+      ],
+    });
+
+    return {
+      tokenAddress,
+      encodedGameData,
+      encodedTxData: encodedData,
+    };
+  }, [formValues?.horse, formValues?.wager]);
+
+  const handleTx = useHandleTx<typeof controllerAbi, "perform">({
+    writeContractVariables: {
+      abi: controllerAbi,
+      functionName: "perform",
+      args: [
+        gameAddresses.horseRace,
+        encodedParams.tokenAddress,
+        uiOperatorAddress as Address,
+        "claim",
+        encodedParams.encodedGameData,
+      ],
+      address: controllerAddress as Address,
+    },
+    options: {},
+    encodedTxData: encodedParams.encodedTxData,
+  });
+
+  const encodedClaimParams = useMemo(() => {
+    const encodedChoice = encodeAbiParameters([], []);
+
+    const encodedParams = encodeAbiParameters(
+      [
+        { name: "address", type: "address" },
+        {
+          name: "data",
+          type: "address",
+        },
+        {
+          name: "bytes",
+          type: "bytes",
+        },
+      ],
+      [
+        currentAccount.address || "0x0000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000",
+        encodedChoice,
+      ]
+    );
+
+    const encodedClaimData: `0x${string}` = encodeFunctionData({
+      abi: controllerAbi,
+      functionName: "perform",
+      args: [
+        gameAddresses.horseRace as Address,
+        "0x0000000000000000000000000000000000000001",
+        uiOperatorAddress as Address,
+        "claim",
+        encodedParams,
+      ],
+    });
+
+    return {
+      encodedClaimData,
+      encodedClaimTxData: encodedClaimData,
+      currentAccount,
+    };
+  }, [formValues.horse, formValues.wager]);
+
+  const handleClaimTx = useHandleTx<typeof controllerAbi, "perform">({
+    writeContractVariables: {
+      abi: controllerAbi,
+      functionName: "perform",
+      args: [
+        gameAddresses.horseRace,
+        encodedParams.tokenAddress,
+        uiOperatorAddress as Address,
+        "claim",
+        encodedClaimParams.encodedClaimData,
+      ],
+      address: controllerAddress as Address,
+    },
+    options: {},
+    encodedTxData: encodedClaimParams.encodedClaimTxData,
   });
 
   const onGameSubmit = async () => {
@@ -58,13 +191,80 @@ const HorseRaceGame = (props: TemplateWithWeb3Props) => {
 
       if (!handledAllowance) return;
     }
+
+    console.log("submit");
+    try {
+      await handleClaimTx.mutateAsync();
+    } catch (error) {}
+
+    try {
+      await handleTx.mutateAsync();
+    } catch (e: any) {
+      console.log("error", e);
+    }
   };
 
   useEffect(() => {
     if (!gameEvent) return;
 
-    console.log(gameEvent);
-  }, [gameEvent]);
+    console.log("gameEvent:", gameEvent);
+
+    const currentTime = new Date().getTime() / 1000;
+
+    let status: MultiplayerGameStatus = MultiplayerGameStatus.None;
+
+    const {
+      cooldownFinish,
+      joiningFinish,
+      joiningStart,
+      randoms,
+      result,
+      player,
+      bet,
+      participants,
+      isGameActive,
+    } = gameEvent;
+
+    const isGameFinished =
+      currentTime >= joiningFinish && joiningFinish > 0 && randoms;
+    const shouldWait =
+      currentTime <= joiningFinish && currentTime >= joiningStart;
+
+    if (shouldWait) {
+      updateState({
+        startTime: joiningFinish,
+        finishTime: cooldownFinish,
+        status: HorseRaceStatus.Started,
+      });
+    }
+    if (isGameFinished) {
+      updateState({
+        status: HorseRaceStatus.Finished,
+        winnerHorse: result,
+      });
+    }
+
+    console.log("status:", { status });
+
+    // if (participants?.length > 0 && isGameActive) {
+    //   participants?.forEach((p) => {
+    //     const _participantHorse =
+    //       horseRaceParticipantMapWithStore[p.selection as unknown as Horse];
+
+    //     setSelectedHorse(_participantHorse, {
+    //       bet: p.amountInUsd,
+    //       name: p.address,
+    //     });
+    //   });
+    // }
+
+    // if (bet && bet?.converted?.wager && player) {
+    //   setWheelParticipant(participantMapWithStore[bet.choice] as Multiplier, {
+    //     player: player,
+    //     bet: bet.converted.wager,
+    //   });
+    // }
+  }, [gameEvent, currentAccount.address]);
 
   return (
     <div>
