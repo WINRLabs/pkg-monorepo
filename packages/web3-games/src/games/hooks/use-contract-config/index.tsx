550f19c3f1d6e6fa106bc0d810ce42cda002e379
WINRLabs <justb.serbia@gmail.com>
2024-10-11 09:42:06 +0300

    Revert "Custom multicall (#344)"
    This reverts commit ff5327587f41fe74800a638e3879aebe5c8824e6.

--- packages/web3-games/src/games/hooks/use-contract-config/index.tsx
+++ packages/web3-games/src/games/hooks/use-contract-config/index.tsx
@@ -7,7 +7,6 @@ export interface ContractConfig {
   cashierAddress: `0x${string}`;
   uiOperatorAddress: `0x${string}`;
   rankMiddlewareAddress: `0x${string}`;
-  multiCallAddress: `0x${string}`;
 }
 
 export interface GameAddresses {
@@ -71,7 +70,6 @@ const ContractConfigContext = React.createContext<ContractConfigContext>({
   cashierAddress: '0x',
   uiOperatorAddress: '0x',
   rankMiddlewareAddress: '0x',
-  multiCallAddress: '0xca11bde05977b3631167028862be2a173976ca11',
 });
 
 export const useContractConfigContext = () => {
