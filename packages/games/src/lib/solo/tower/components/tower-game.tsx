c4a7f9e72bda18e8d4a09b3348305c54f7ab316e
WINRLabs <justb.serbia@gmail.com>
2024-10-14 19:36:54 +0300

    📝 (tower-game.tsx): Remove unused import statement to clean up code ♻️ (tower-game.tsx): Refactor generateGrid function to create a 4x8 grid with a 25% chance of a bomb 🐛 (tower-game.tsx): Fix logic in handleClick function to correctly update grid and handle bomb clicks 🐛 (tower-game.tsx): Fix logic in getBackgroundImage function to display correct images based on game state 🐛 (tower-game.tsx): Fix styling and functionality issues in the TowerGame component

--- packages/games/src/lib/solo/tower/components/tower-game.tsx
+++ packages/games/src/lib/solo/tower/components/tower-game.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 
-import { cn } from '../../../utils/style';
 import { TemplateOptions } from './template';
 
 interface Cell {
@@ -14,10 +13,9 @@ export type TowerGameProps = {
 };
 
 const generateGrid = (): Cell[][] => {
-  // Generate a 3x8 grid with random bomb placements
-  const grid: Cell[][] = Array.from({ length: 3 }, () =>
+  const grid: Cell[][] = Array.from({ length: 4 }, () =>
     Array.from({ length: 8 }, () => ({
-      isBomb: Math.random() < 0.5, // 30% chance of bomb
+      isBomb: Math.random() < 0.25, // 25% chance of bomb
       isClickable: false,
       isSelected: false,
     }))
@@ -36,34 +34,36 @@ const TowerGame = ({ ...props }: TowerGameProps) => {
 
   const [grid, setGrid] = useState<Cell[][]>(generateGrid);
   const [gameOver, setGameOver] = useState(false);
+  const [hoveredCell, setHoveredCell] = useState<{ row: number; col: number } | null>(null);
 
   const handleClick = (row: number, col: number) => {
     if (!grid[row]?.[col]?.isClickable || gameOver) return;
 
-    if (grid[row][col].isBomb) {
+    const newGrid = grid.map((rowArr, rowIndex) =>
+      rowArr.map((cell, colIndex) => {
+        if (rowIndex === row && colIndex === col) {
+          return { ...cell, isSelected: true };
+        }
+        if (colIndex === col + 1) {
+          return { ...cell, isClickable: true };
+        }
+        if (colIndex === col) {
+          return { ...cell, isClickable: false };
+        }
+        return cell;
+      })
+    );
+
+    if (newGrid[row]?.[col]?.isBomb) {
       // Bomb clicked, game over
       setGameOver(true);
       alert('Game Over! You hit a bomb!');
-    } else {
-      // Make the next column clickable and disable the current column
-
-      const newGrid = grid.map((rowArr, rowIndex) =>
-        rowArr.map((cell, colIndex) => {
-          if (colIndex === col + 1) {
-            return { ...cell, isClickable: true };
-          }
-          if (colIndex === col) {
-            return { ...cell, isClickable: false };
-          }
-          return cell;
-        })
+      // Set all cells to not clickable
+      const updatedGrid = newGrid.map((rowArr) =>
+        rowArr.map((cell) => ({ ...cell, isClickable: false }))
       );
-
-      // Select the clicked cell
-      if (newGrid[row] && newGrid[row][col]) {
-        newGrid[row][col].isSelected = true;
-      }
-
+      setGrid(updatedGrid);
+    } else {
       setGrid(newGrid);
     }
   };
@@ -73,6 +73,30 @@ const TowerGame = ({ ...props }: TowerGameProps) => {
     setGameOver(false);
   };
 
+  const getBackgroundImage = React.useCallback(
+    (rowIndex: number, colIndex: number, grid: Cell[][], gameOver: boolean): string => {
+      if (
+        grid[rowIndex]?.[colIndex]?.isBomb &&
+        grid[rowIndex]?.[colIndex]?.isSelected &&
+        gameOver
+      ) {
+        return `url(${options.scene?.bombCell})`;
+      } else if (grid[rowIndex]?.[colIndex]?.isBomb && gameOver) {
+        return `url(${options.scene?.cellBomb})`;
+      } else if (grid[rowIndex]?.[colIndex]?.isSelected) {
+        return `url(${options.scene?.selectedCell})`;
+      } else if (!grid[rowIndex]?.[colIndex]?.isClickable) {
+        return `url(${options.scene?.cell})`;
+      } else if (hoveredCell?.row === rowIndex && hoveredCell?.col === colIndex) {
+        return `url(${options.scene?.cellHover})`;
+      } else if (grid[rowIndex]?.[colIndex]?.isClickable) {
+        return `url(${options.scene?.hoverRowCell})`;
+      }
+      return '';
+    },
+    [options.scene, hoveredCell, gameOver]
+  );
+
   return (
     <div className=" wr-w-full wr-h-full wr-relative">
       <img
@@ -81,51 +105,43 @@ const TowerGame = ({ ...props }: TowerGameProps) => {
         className="wr-w-full wr-h-full  wr-absolute wr-z-10"
       />
       <div className="lg:wr-mb-3"></div>
-      <div className="wr-flex wr-flex-col wr-items-center  wr-z-20 wr-relative ">
-        <h1 className="wr-text-2xl wr-font-bold wr-mb-4">Tower Game</h1>
-        {gameOver && <div className="wr-text-red-500 wr-font-bold">Game Over!</div>}
-        <div className="wr-grid wr-grid-rows-8 wr-gap-4">
+      <div className="wr-flex wr-flex-col wr-items-center  wr-justify-center wr-h-full wr-z-20 wr-relative ">
+        <div
+          className="wr-grid wr-grid-rows-8 wr-gap-4 wr-p-6"
+          style={{ backgroundImage: `url(${options.scene?.gameBg})` }}
+        >
           {grid[0]?.map((_, colIndex) => (
             <div
               key={colIndex}
-              className="wr-grid wr-grid-cols-3 wr-gap-2"
+              className="wr-grid wr-grid-cols-4 wr-gap-2"
               style={{ order: -colIndex }}
             >
-              {grid.map((_, rowIndex) => (
-                <button
-                  key={`${rowIndex}-${colIndex}`}
-                  className={cn(
-                    'wr-w-44 wr-h-14 wr-rounded-md wr-grid wr-place-items-center wr-text-white wr-font-bold wr-transition-all wr-duration-300',
-                    {
-                      'wr-bg-blue-500 hover:wr-bg-blue-700':
-                        grid[rowIndex]?.[colIndex]?.isClickable,
-                      'wr-bg-zinc-900': !grid[rowIndex]?.[colIndex]?.isClickable,
-                      'wr-bg-lime-600': grid[rowIndex]?.[colIndex]?.isSelected,
-                      'wr-bg-red-600': grid[rowIndex]?.[colIndex]?.isBomb && gameOver,
-                    }
-                  )}
-                  onClick={() => handleClick(rowIndex, colIndex)}
-                  disabled={!grid[rowIndex]?.[colIndex]?.isClickable}
-                >
-                  {gameOver && grid[rowIndex]?.[colIndex]?.isBomb ? (
-                    <img src={options.scene?.bombImage} alt="bomb" width={60} height={60} />
-                  ) : grid[rowIndex]?.[colIndex]?.isSelected ? (
-                    <img src={options.scene?.gemImage} alt="selected" width={60} height={60} />
-                  ) : (
-                    <img src={options.scene?.logo} alt="Click Me!" width={40} height={40} />
-                  )}
-                </button>
-              ))}
+              {grid.map((_, rowIndex) => {
+                return (
+                  <button
+                    onMouseEnter={() => setHoveredCell({ row: rowIndex, col: colIndex })}
+                    onMouseLeave={() => setHoveredCell(null)}
+                    key={`${rowIndex}-${colIndex}`}
+                    style={{
+                      backgroundImage: getBackgroundImage(rowIndex, colIndex, grid, gameOver),
+                      transition: 'background-image 0.3s ease-in-out',
+                    }}
+                    className="wr-w-[68px] wr-h-9 wr-grid wr-place-items-center wr-bg-no-repeat wr-bg-center wr-bg-contain wr-text-white wr-font-bold wr-transition-all wr-duration-300"
+                    onClick={() => handleClick(rowIndex, colIndex)}
+                    disabled={!grid[rowIndex]?.[colIndex]?.isClickable}
+                  />
+                );
+              })}
             </div>
           ))}
         </div>
-        <button
-          className="wr-mt-4 wr-px-4 wr-py-2 wr-bg-blue-500 wr-text-white wr-font-bold wr-rounded-md hover:wr-bg-blue-700"
-          onClick={resetGame}
-        >
-          Reset Game
-        </button>
       </div>
+      <button
+        className="wr-mt-4 wr-px-4 wr-py-2 wr-bg-blue-500 wr-text-white wr-font-bold wr-rounded-md hover:wr-bg-blue-700"
+        onClick={resetGame}
+      >
+        Reset Game
+      </button>
     </div>
   );
 };
