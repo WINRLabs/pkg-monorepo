9b76bf98a5410c6320247f379c470edcac8e7022
WINRLabs <justb.serbia@gmail.com>
2024-10-30 12:35:48 +0300

    ✨ (page.tsx): Remove unnecessary wrapper div from CoinFlipGame component to simplify the structure ♻️ (globals.css): Update CSS selectors to remove the wrapper class and improve styling consistency

--- apps/web/app/game/coin-flip-2d/page.tsx
+++ apps/web/app/game/coin-flip-2d/page.tsx
@@ -5,18 +5,15 @@ import { CoinFlipGame, LiveResults, Web3GamesModals } from '@winrlabs/web3-games
 export default function CoinFlipPage() {
   return (
     <>
-      <div className="wrapper">
-        <CoinFlipGame
-          options={{
-            scene: {
-              backgroundImage: 'url(/coin-flip/coin-flip-bg.png)',
-            },
-          }}
-          minWager={0.01}
-          maxWager={2000}
-        />
-      </div>
-
+      <CoinFlipGame
+        options={{
+          scene: {
+            backgroundImage: 'url(/coin-flip/coin-flip-bg.png)',
+          },
+        }}
+        minWager={0.01}
+        maxWager={2000}
+      />
       <LiveResults />
       <Web3GamesModals />
     </>
