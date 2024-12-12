5ef1e1a611b0d1fa35d13e218754f46816b4cc7a
WINRLabs <justb.serbia@gmail.com>
2024-12-12 12:11:13 +0300

    add closed trades

--- packages/games/src/lib/multiplayer/crash/components/scene/index.tsx
+++ packages/games/src/lib/multiplayer/crash/components/scene/index.tsx
@@ -6,5 +6,6 @@ export default function Scene() {
   const [sceneElementRef, setSceneElementRef] = useState<HTMLElement | null>(null);
   useCrashGame({ elementRef: sceneElementRef });
 
+  // todo: what the heck is this?
   return <div ref={(ref) => setSceneElementRef(ref)}></div>;
 }
