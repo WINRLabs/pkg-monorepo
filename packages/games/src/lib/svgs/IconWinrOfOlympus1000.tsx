a701930d2ae17e1ed91060d294c3e384c79201d5
WINRLabs <117850738+WINRLabs@users.noreply.github.com>
2024-12-16 02:52:52 +0300

    Fix/winr bonanza 1000 remove super (#460)
    * winr bonanza 1000 update
    
    * add icons
    
    * changeset

---
+++ packages/games/src/lib/svgs/IconWinrOfOlympus1000.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+
+const SvgIconWinrOfOlympus1000 = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="none"
+    {...props}
+  >
+    <g clipPath="url(#icon-winr-of-olympus-1000_svg__a)">
+      <path
+        fill="currentColor"
+        d="M14.31 10.59h-2.358l2.827-3.769a.586.586 0 0 0-.469-.937h-1.586L14.095.737a.586.586 0 0 0-1.01-.532L6.023 8.442a.586.586 0 0 0 .445.967h1.577l-2.827 3.77a.586.586 0 0 0 .469.937h1.586l-1.371 5.147a.586.586 0 0 0 .99.555l7.842-8.237a.586.586 0 0 0-.425-.99"
+      />
+      <path
+        fill="currentColor"
+        d="M5.5 13a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11"
+      />
+      <path
+        fill="#09090B"
+        d="m4.645 7.443-1.633-2.51h1.476l1.017 1.743 1.017-1.742h1.467l-1.653 2.51 1.73 2.623H6.6L5.505 8.2 4.4 10.067H2.934z"
+      />
+    </g>
+    <defs>
+      <clipPath id="icon-winr-of-olympus-1000_svg__a">
+        <path fill="#fff" d="M0 0h20v20H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default SvgIconWinrOfOlympus1000;
